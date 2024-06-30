4aaf699d2e0552b948558d24602c2ff88763cb84
Rebel-Fox <rookiewebdev@outlook.com>
2024-06-30 14:00:19 +0530

    modified css

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import {nanoid} from 'nanoid'
+import './die.css'
 
 export default function Die(props) {
+
+    const dots=[]
+
+    for(let i=0;i<props.value;i++){
+        dots.push(<span key= {nanoid()} className='dot'></span>)
+    }
+
     return (
-        <button className={`box ${props.isHeld ? 'held':''}`} onClick={props.holdDice}>{props.value} </button>
+        <button className={`box ${props.isHeld ? 'held':''}`} onClick={props.holdDice}>{dots} </button>
     )
 }
