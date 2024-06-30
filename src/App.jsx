5ca98180462ea40a536d1944eb05b77d35b9789b
Rebel-Fox <rookiewebdev@outlook.com>
2024-06-30 17:03:50 +0530

    added timer and highscore

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import Confetti from 'react-confetti'
 
 export default function App() {
 
+  const highscore = JSON.parse(localStorage.getItem('Highscore'))
+
   const [numArr, setNumArr] = React.useState(allNewDice())
 
   const [tenzies, setTenzies] = React.useState(false)
 
+  const [numRoll, setNumRoll] = React.useState(0)
+
+  const [time, setTime] = React.useState(0)
   React.useEffect(() => {
     const numValue = numArr[0].value
     const isAllHeld = numArr.every(num => num.isHeld) // this returns a boolean
@@ -17,6 +22,16 @@ export default function App() {
       setTenzies(true)
     }
   }, [numArr])
+
+  React.useEffect(() => {
+    let timer
+    if(!tenzies){
+      timer =setInterval(() => setTime(prevTime => prevTime + 1),1000)
+    }else{
+      clearInterval(timer)
+    }
+    return () => clearInterval(timer)
+  },[tenzies])
 
   function newDice() {
     const randomNum = Math.floor(Math.random() * 6) + 1
@@ -52,25 +67,39 @@ export default function App() {
     />)
 
   function generateNewNumArr() {
-    if(tenzies){
+    if (tenzies) {
+      if (highscore) {
+        if (highscore > numRoll) {
+          localStorage.setItem('Highscore', JSON.stringify(numRoll))
+        }
+      } else {
+        localStorage.setItem('Highscore', JSON.stringify(numRoll))
+      }
+
       setTenzies(false)
       setNumArr(allNewDice())
-    } else{
+      setNumRoll(0)
+      setTime(0)
+    } else {
+      setNumRoll(prevNumRoll => prevNumRoll + 1)
       setNumArr(oldNumArr => oldNumArr.map(oldNum => oldNum.isHeld ? oldNum : newDice()))
     }
   }
 
 
-
   return (
     <main>
+
+        <span className='counter'>Roll: {numRoll}</span>
+        {highscore && <span className='highscore'>Highscore:{highscore}</span>}
+        {time!==0 && <span className='timer'>{time}s</span>}
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="boxes">
         {numElements}
       </div>
-      <button className='roll-btn' onClick={generateNewNumArr}>{tenzies ? 'New Game': 'Roll'}</button>
-      {tenzies && <Confetti/>}
+      <button className='roll-btn' onClick={generateNewNumArr}>{tenzies ? 'New Game' : 'Roll'}</button>
+      {tenzies && <Confetti />}
     </main>
   )
 }
